c54737fe83f3f6c38f7b98d08cd22de8c651f588
Fatma ÇİÇEK <fatmacicek0144@gmail.com>
2024-05-22 21:09:00 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
        <Navbar/>
+       <
+       <HeroSection/>
     </>
   )
 }
